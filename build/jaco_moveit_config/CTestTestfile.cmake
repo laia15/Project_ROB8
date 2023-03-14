600f0c9a0f7e38434ee98ae642cc7ecd1856be2e
Laia <lvives22@student.aau.dk>
2023-03-14 09:30:46 +0100

    initial commit

---
+++ build/jaco_moveit_config/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/laia/project_ws/src/jaco_moveit_config
+# Build directory: /home/laia/project_ws/build/jaco_moveit_config
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/project_group852/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/laia/project_ws/src/project_group852
+# Build directory: /home/laia/project_ws/build/project_group852
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
